d5bc8e6ea027ad8c7a4e8c9ca7f1c52ad55aef8d
Monabzh <mona.breton44@gmail.com>
2026-07-24 16:58:59 +0200

    tableau complet sans fix type

---
+++ tableau/script.js
@@ -0,0 +1,36 @@
+grist.ready({ requiredAccess: 'read table'});
+
+grist.onRecords(function (records) {
+    // conteneur
+    const listeEl = document.getElementById('liste');
+    listeEl.replaceChildren();
+
+    // tableau dans conteneur
+    const table = document.createElement('table');
+    listeEl.appendChild(table);
+
+    // listes des colonnes
+    const colonnes = Object.keys(records[0]).filter(function (nom) {
+        return nom !== 'id';
+    });
+
+    // En-tête
+    const trEntete = document.createElement('tr');
+    colonnes.forEach(function (nom) {
+        const th = document.createElement('th');
+        th.textContent = nom;
+        trEntete.appendChild(th);
+    });
+    table.appendChild(trEntete);
+    
+    // lignes
+    records.forEach(function (record) {
+        const tr = document.createElement('tr');
+        colonnes.forEach(function (nom) {
+            const td = document.createElement('td');
+            td.textContent = record[nom];
+            tr.appendChild(td);
+        });
+        table.appendChild(tr);
+    });
+});
