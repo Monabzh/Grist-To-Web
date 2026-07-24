bec5886871be5faa3d1132a1add1bcf8ba5a9b19
Monabzh <mona.breton44@gmail.com>
2026-07-24 14:36:29 +0200

    fix conteneur

--- bibliotheque/script.js
+++ bibliotheque/script.js
@@ -5,9 +5,10 @@ grist.ready({ requiredAccess: 'read table'});
 grist.onRecords(function (records) {
     statusEl.textContent = "Connecté -" + records.length + "lignes";
     console.log(records);
-    const listeEl = document.getElementById('liste');
+
     records.forEach(function (record){
-        ele = record.A + " " + record.B + " - " + record.C;
-        listeEl.appendChild(ele)
+        const ligneEl = document.createElement('div');
+        const ligneEl.textContent = record.A + " " + record.B + " - " + record.C;
+        listeEl.appendChild(ligneEl);
     })
 });
