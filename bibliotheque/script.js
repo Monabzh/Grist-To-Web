d763b9dab4a40ca875740f79518d6d0f57f31583
Monabzh <mona.breton44@gmail.com>
2026-07-24 15:58:41 +0200

    données tableaux

--- bibliotheque/script.js
+++ bibliotheque/script.js
@@ -6,9 +6,12 @@ grist.onRecords(function (records) {
     statusEl.textContent = "Connecté -" + records.length + "lignes";
     console.log(records);
 
+    const listeEl = document.getElementById('liste');
+    listeEl.replaceChildren();
+    
     records.forEach(function (record){
         const ligneEl = document.createElement('div');
         ligneEl.textContent = record.A + " " + record.B + " - " + record.C;
-        ligneEl.appendChild(ligneEl);
+        listeEl.appendChild(ligneEl);
     })
 });
