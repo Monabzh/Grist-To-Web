91c503f9bc1d04caa7c6e3792645a05fed0662af
Monabzh <mona.breton44@gmail.com>
2026-07-24 14:25:47 +0200

    afficher données par colonnes

--- bibliotheque/script.js
+++ bibliotheque/script.js
@@ -5,4 +5,9 @@ grist.ready({ requiredAccess: 'read table'});
 grist.onRecords(function (records) {
     statusEl.textContent = "Connecté -" + records.length + "lignes";
     console.log(records);
+    const listeEl = document.getElementById('liste');
+    records.forEach(function (record){
+        ele = record.A + " " + record.B + " - " + record.C;
+        listeEl.appendChild(ele)
+    })
 });
